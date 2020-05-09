b8617a353977cfff1a2bd9311211cf42c8544744
wasim <wasim.afser@gmail.com>
2020-05-09 18:56:08 +0530

    Initialize base

---
+++ service.py
@@ -0,0 +1,38 @@
+import win32serviceutil
+import win32service
+import win32event
+import servicemanager
+
+import socket
+import logging
+
+logging.basicConfig(filename="service.log", level=logging.DEBUG)
+
+
+class ExampleService(win32serviceutil.ServiceFramework):
+    _svc_name_ = "AExampleService"
+    _svc_display_name_ = "AExample Service"
+    _svc_description_ = "Example description"
+
+    def __init__(self, args):
+        win32serviceutil.ServiceFramework.__init__(self, args)
+        self.hWaitStop = win32event.CreateEvent(None, 0, 0, None)
+        socket.setdefaulttimeout(60)
+
+    def SvcDoRun(self):
+        servicemanager.LogMsg(servicemanager.EVENTLOG_INFORMATION_TYPE,
+                              servicemanager.PYS_SERVICE_STARTED,
+                              (self._svc_name_, ''))
+        self.main()
+
+    def SvcStop(self):
+        self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
+        win32event.SetEvent(self.hWaitStop)
+
+    def main(self):
+        """DO THE RUN STUFF HERE"""
+        logging.info("SERVICE MAIN ENTERED !!")
+
+
+if __name__ == '__main__':
+    win32serviceutil.HandleCommandLine(ExampleService)

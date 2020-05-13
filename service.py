372fc283d04f87198c091186a20273c6c656dffc
wasim <wasim.afser@gmail.com>
2020-05-13 08:37:42 +0530

    initialize CreateProcessAsUser method

--- service.py
+++ service.py
@@ -2,6 +2,10 @@ import win32serviceutil
 import win32service
 import win32event
 import servicemanager
+import win32ts
+import win32profile
+import win32process
+import win32con
 
 import socket
 import logging
@@ -34,7 +38,34 @@ class ExampleService(win32serviceutil.ServiceFramework):
     def main(self):
         """DO THE RUN STUFF HERE"""
         logging.info("SERVICE MAIN ENTERED !!")
-        psutil.Popen('notepad.exe')
+        # 1. GET USER TOKEN
+        console_session_id = win32ts.WTSGetActiveConsoleSessionId()
+        console_user_token = win32ts.WTSQueryUserToken(console_session_id)
+
+        # 2. GET THIS USER'S ENVIRONMENT
+        environment = win32profile.CreateEnvironmentBlock(console_user_token, False)
+
+        # 3. GENERATE STARTUPINFO FOR THE PROCESS ( OPTIONAL )
+        startupInfo = win32process.STARTUPINFO()
+        # OPTIONAL
+        startupInfo.dwFlags = win32process.STARTF_USESHOWWINDOW
+        startupInfo.wShowWindow = win32con.SW_NORMAL
+
+        # 4. CREATE PROCESS AS USER
+        win32process.CreateProcessAsUser(console_user_token,
+                                         'notepad.exe',
+                                         None,
+                                         None,
+                                         None,
+                                         0,
+                                         win32con.NORMAL_PRIORITY_CLASS,
+                                         environment,
+                                         None,
+                                         startupInfo)
+
+
+
+        # psutil.Popen('notepad.exe')
 
 
 if __name__ == '__main__':
